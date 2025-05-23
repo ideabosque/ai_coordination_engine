63d30325adf2b050a501a970d1f61f84ef3c8a39
Idea Bosque <ideabosque@gmail.com>
2025-05-22 20:28:33 -0700

    Fix the endpoint_id.

--- ai_coordination_engine/models/coordination.py
+++ ai_coordination_engine/models/coordination.py
@@ -76,7 +76,9 @@ def get_coordination_type(
 
 
 def resolve_coordination(info: ResolveInfo, **kwargs: Dict[str, Any]) -> Any:
-    count = get_coordination_count(kwargs["endpoint_id"], kwargs["coordination_uuid"])
+    count = get_coordination_count(
+        info.context["endpoint_id"], kwargs["coordination_uuid"]
+    )
     if count == 0:
         return None
 
