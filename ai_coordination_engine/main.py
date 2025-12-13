cb4613f639d360ce6c929e099e1dbc9b0d64581b
Idea Bosque <ideabosque@gmail.com>
2025-12-12 21:56:14 -0800

    Update code

--- ai_coordination_engine/main.py
+++ ai_coordination_engine/main.py
@@ -9,6 +9,7 @@ from typing import Any, Dict, List
 
 from graphene import Schema
 
+from silvaengine_dynamodb_base import BaseModel
 from silvaengine_utility import Graphql
 
 from .handlers.config import Config
@@ -151,6 +152,15 @@ def deploy() -> List:
 class AICoordinationEngine(Graphql):
     def __init__(self, logger: logging.Logger, **setting: Dict[str, Any]) -> None:
         Graphql.__init__(self, logger, **setting)
+
+        if (
+            setting.get("region_name")
+            and setting.get("aws_access_key_id")
+            and setting.get("aws_secret_access_key")
+        ):
+            BaseModel.Meta.region = setting.get("region_name")
+            BaseModel.Meta.aws_access_key_id = setting.get("aws_access_key_id")
+            BaseModel.Meta.aws_secret_access_key = setting.get("aws_secret_access_key")
 
         # Initialize configuration via the Config class
         Config.initialize(logger, **setting)
