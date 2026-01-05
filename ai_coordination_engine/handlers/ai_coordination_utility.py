e01ea4abc4ab7b26b827de460230c3ff1b61d2b6
Barry Liu <barry.y.liu@hotmail.com>
2026-01-05 13:00:40 +0800

    [U] Optimization

--- ai_coordination_engine/handlers/ai_coordination_utility.py
+++ ai_coordination_engine/handlers/ai_coordination_utility.py
@@ -30,7 +30,7 @@ def execute_graphql_query(
 ) -> Dict[str, Any]:
     schema = Config.fetch_graphql_schema(context, function_name)
     context.get("logger").info(f"{'=' * 40} Schema Start {'>' * 40}")
-    context.get("logger").info(variables)
+    context.get("logger").info(schema)
     context.get("logger").info(f"{'=' * 40} Schema End {'>' * 40}")
     context.get("logger").info(f"{'=' * 40} Function Name Start {'>' * 40}")
     context.get("logger").info(function_name)
