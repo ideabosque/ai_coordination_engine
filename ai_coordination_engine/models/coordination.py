9c5a83aa53e23f42f80fdc38bd2a0f639195cd21
Barry Liu <barry.y.liu@hotmail.com>
2026-04-08 17:08:52 +0800

    [U] Fixed issue

--- ai_coordination_engine/models/coordination.py
+++ ai_coordination_engine/models/coordination.py
@@ -147,7 +147,7 @@ def resolve_coordination(
 
 @monitor_decorator
 @resolve_list_decorator(
-    attributes_to_get=["endpoint_id", "coordination_uuid"],
+    attributes_to_get=["endpoint_id", "coordination_uuid", "partition_key"],
     list_type_class=CoordinationListType,
     type_funct=get_coordination_type,
 )
